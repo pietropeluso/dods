47e3e0606204426c49413f08023bcc3c5139cf9c
Pietro Peluso <pietro.peluso@gmail.com>
2014-04-14 15:38:07 +0200

    modified data fetching

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,7 @@
 
 angular.module('dodsApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+    $http.get('/api/dataset').success(function(data) {
+      $scope.dataset = data;
     });
   });
