0d1e4304e5e2b401250e52d9c9b14661e73027ae
Pietro Peluso <pietro.peluso@gmail.com>
2014-04-14 17:06:00 +0200

    using factory to fetch data instead of controller, so different controllers get data from the same factory

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,20 @@
 'use strict';
 
 angular.module('dodsApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/dataset').success(function(data) {
-      $scope.dataset = data;
-    });
+  .controller('MainCtrl', function ($scope, $http, dodsFactory) {
+
+    $scope.dataset = [];
+
+    var init = function() {
+      dodsFactory.getPeople().success(function(data){
+        $scope.dataset=data;
+      });
+    }
 
     $scope.toHighlight = function(suspend) {
       return suspend;
     };
+
+    init();
 
   });
---
+++ app/scripts/services/dods-factory.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('dodsApp')
+  .factory('dodsFactory', function ($http) {
+    // Public API here
+    return{
+      getPeople : function() {
+        return $http({
+          url: '/api/dataset',
+          method: 'GET'
+        })
+      }
+    }
+  });
